f169ab497a204239d02262813d1d361a8c2bcca4
吴鑫程 <112101054+Xclince@users.noreply.github.com>
2025-03-07 18:13:47 +0800

    Update navbar.ts

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -9,11 +9,11 @@ export default navbar([
     prefix: "/posts/",
     children: [
       {
-        text: "苹果",
+        text: "年度总结",
         icon: "pen-to-square",
         prefix: "apple/",
         children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
+          { text: "回首2024，展望2025", icon: "pen-to-square", link: "1" },
           { text: "苹果2", icon: "pen-to-square", link: "2" },
           "3",
           "4",
